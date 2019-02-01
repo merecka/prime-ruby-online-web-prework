3824bceba2b319829938efd9cef612b59666817e
Alex Merecka <merecka@gmail.com>
2019-02-01 03:15:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+  return false
+  else
+  prime_array = (2..Math.sqrt(number)).to_a
+  prime_array.each{|x| return false if number % x == 0}
+  return true
+  end
+end
