28a2d7fc87047f655dc3881f1af826608b82db6e
Alex Merecka <merecka@gmail.com>
2019-02-01 03:11:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,9 @@
 def prime?(number)
   if number <= 1
   return false
+  elseif
+    number == 2
+    return true
   else
   prime_array = (2..number).to_a
   prime_array.each{|x| return false if number % x == 0}
